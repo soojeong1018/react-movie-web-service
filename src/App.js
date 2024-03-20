33c928372f087d4d380e490fef7ed07354cdc5ed
ksj <tnwjd940412@gmail.com>
2024-03-20 23:56:38 +0900

    movie 1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
 import {useState} from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const onChange = (event) => setToDo(event.target.value);
-    const [toDos, setToDos] = useState([]);
-    const onSubmit = (event) => {
-      event.preventDefault();
-      if (toDo === "") {
-          return;
-      }
-
-      setToDos(currentArr => [toDo, ...currentArr]);
-      setToDo("");
-    };
-
-    console.log(toDos);
-    console.log(toDos.map((item, i)=> {
-        return <li key={i}>{item}</li>
-    }));
-
-    return (
-        <div>
-            <h1>Todos ({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    type="text"
-                    value={toDo}
-                    placeholder="Write your to do..."/>
-                <button>Add To Do</button>
-            </form>
-            <hr/>
-            <ul>
-                {toDos.map((item, i)=> {
-                    return <li key={i}>{item}</li>
-                })}
-            </ul>
-        </div>
-    );
+    return null;
 }
 
 export default App;
