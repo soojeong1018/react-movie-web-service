17657c2d6ec0a561d6820328396021a9eaaa32b3
kimsoojeong <tnwjd18412@softworks.co.kr>
2024-03-23 23:44:11 +0900

    route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import {useState} from "react";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom"
 
 function App() {
-    return null;
+    return <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail" element={<Detail />} />
+        </Routes>
+    </Router>;
 }
 
 export default App;
